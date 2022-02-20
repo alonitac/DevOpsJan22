e6172d5ab61e8b35e714ed8400a5d0a9bd4d2312
netanalm <98554254+netanalm@users.noreply.github.com>
2022-02-20 20:19:13 +0200

    nati's solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,9 +1,8 @@
-wget https://devops-jan22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
-tar -xzvf secretGenerator.tar.gz
+cd src
 mkdir secretDir
 touch secretDir/.secret
 chmod 600 secretDir/.secret
-mkdir maliciousFilesnew
-mv /home/nati/src/maliciousFiles/* /home/nati/maliciousFilesnew
+rm -r maliciousFiles/
+rm impotrent.link
 /bin/bash /home/nati/src/generateSecret.sh
 cat /home/nati/secretDir/.secret
