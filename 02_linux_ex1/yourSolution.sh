ba46d7c16788f0885e9f61723f15733b713f0949
dmitriyshub <d.shtranvasser@gmail.com>
2022-02-26 02:19:10 +0200

    adding solution bash script

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,12 @@
-#My solution:
+#! /bin/bash
+
+wget https://devops-jan22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
+tar -xf secretGenerator.tar.gz
+cd src
+rm -r maliciousFiles/ important.link
+mkdir secretDir
+touch secretDir/.secret
+chmod 774 secretDir && chmod 600 secretDir/.secret
+/bin/bash generateSecret.sh
+printf "This is my secret: "
+cat secretDir/.secret
