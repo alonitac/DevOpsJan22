019100bbc7a27f40d6adfd26d8b42862347f3cb7
NEWCO\znagar <zohar.nagar@nex.com>
2022-03-12 02:46:43 +0200

    Zohar's solution complete

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,6 +1,22 @@
+#! /bin/bash
+directory=secretDir/
+wget https://devops-jan22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
+tar -xf secretGenerator.tar.gz
+rm -r secretGenerator.tar.gz
 cd src/
-mkdir secretDir
-rm -r maliciousFiles/
-touch secretDir/.secret
-chmod 600 secretDir/.secret
-rm important.link
+if  [ -d "$directory" ]; then
+        rm -r maliciousFiles/
+        rm important.link
+        /bin/bash generateSecret.sh
+        printf "My secret is: "
+        cat secretDir/.secret
+else
+        mkdir secretDir
+        rm -r maliciousFiles/
+        touch secretDir/.secret
+        chmod 600 secretDir/.secret
+        rm important.link
+        /bin/bash generateSecret.sh
+        printf "My secret is: "
+        cat secretDir/.secret
+fi
