45c01f901993f09a7e307dad62bb6725f923bd4c
עדן בחסאס <adan10.aa4@gmail.com>
2022-03-12 14:03:33 +0200

    adanbh's solution

---
+++ 02_linux_ex1/yourSolution.sh
@@ -0,0 +1,28 @@
+wget https://devops-jan22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
+tar -xf secretGenerator.tar.gz
+cd src
+/bin/bash generateSecret.sh
+cd
+sudo apt update
+sudo apt install chkrootkit
+chkrootkit
+rm -ri /tmp/postfix.config.yBsrpV
+chkrootkit
+cd src
+/bin/bash generateSecret.sh
+rm -r maliciousFiles/
+/bin/bash generateSecret.sh
+cd secretDir/
+touch secret
+/bin/bash generateSecret.sh
+cd src
+cd secretDir
+chmod 600 secret
+cd
+cd src
+/bin/bash generateSecret.sh
+cd
+find . -xtype l
+find . -xtype l 2>/dev/null
+cd src
+/bin/bash generateSecret.s
