730eccbf9180990b947d83c4f6a484998f96cce4
NEWCO\znagar <zohar.nagar@nex.com>
2022-03-12 03:09:55 +0200

    Zohar's solution complete - suggested 2 solutions because I wanted to cover the 2 options if wget is needed or not.

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+#I suggested 2 solutions, this solution (yourSolution.sh) in case wget is needed.
 directory=secretDir/
 wget https://devops-jan22.s3.eu-north-1.amazonaws.com/secretGenerator.tar.gz
 tar -xf secretGenerator.tar.gz
