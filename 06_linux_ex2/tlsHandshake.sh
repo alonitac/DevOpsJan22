a43708e27023f66e445cda02aa8e8fd87ce82535
Dady <ddady1@gmail.com>
2022-03-15 23:32:58 +0200

    Dady's

---
+++ 06_linux_ex2/tlsHandshake.sh
@@ -0,0 +1,2 @@
+sudo apt install -y aiohttp-wsgi-serve
+curl -X POST -H 'Content-Type: application/json' -d '{"clientVersion": "3.2","message": "Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello -o response.json
