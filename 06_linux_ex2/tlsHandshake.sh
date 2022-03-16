f658c2f6d2c7efcd4c0d18cc1626b60a48194689
kostalubarsky <kosta@tadiran-group.co.il>
2022-03-16 15:08:31 +0200

    cert.pem ok

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,1 +1,7 @@
 curl -X POST -H 'Content-Type: application/json' -d '{"clientVersion": "3.2","message": "Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello -o fromserver.json
+sudo apt install jq
+SESSION_ID=$(jq -r '.sessionID' fromserver.json)
+jq -r '.serverCert' fromserver.json > cert.pem
+wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem cert.pem
+# cert.pem: OK
