b03badce7fabd41700df139928f1fd81017d230d
kostalubarsky <kosta@tadiran-group.co.il>
2022-03-16 16:30:50 +0200

    encrypt from server

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -5,3 +5,6 @@ jq -r '.serverCert' fromserver.json > cert.pem
 wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 openssl verify -CAfile cert-ca-aws.pem cert.pem
 # cert.pem: OK
+head -c 32 /dev/urandom > masterKey.txt
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange -o encryptfromserver.txt
