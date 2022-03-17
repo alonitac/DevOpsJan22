20b6d5af34eadd89e4860c534c14e3c311bf56ff
kostalubarsky <kosta@tadiran-group.co.il>
2022-03-17 14:57:26 +0200

    decrypt

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -5,6 +5,8 @@ jq -r '.serverCert' fromserver.json > cert.pem
 wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 openssl verify -CAfile cert-ca-aws.pem cert.pem
 # cert.pem: OK
-head -c 32 /dev/urandom > masterKey.txt
+openssl rand -hex 16 > masterKey
 MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
-curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange -o encryptfromserver.txt
+curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange  | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
+openssl enc -d -aes-256-cbc -pbkdf2 -k masterKey -in encSampleMsgReady.txt -out answer.txt
