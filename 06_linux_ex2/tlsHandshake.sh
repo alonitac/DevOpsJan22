c1a1452f7fcac82899fb224b5cc34cba89757efa
kostalubarsky <kosta@tadiran-group.co.il>
2022-03-17 15:10:14 +0200

    decrypt

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -10,3 +10,5 @@ MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER 
 curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange  | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
 openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out decrypted_secret.txt
+cat decrypted_secret.txt
+# Hi server, please encrypt me and send to client!
