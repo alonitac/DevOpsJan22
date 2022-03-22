54f98af76850ad5a301d77b8e736b834e3c58a5f
Alon Itach <alon.itach@mail.huji.ac.il>
2022-03-22 16:01:32 +0200

    ex1 feedback

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -2,3 +2,11 @@ cd src
 ls
 cat generateSecret.sh
 cat CONTENT_TO_HASH
+
+# FIXME (A) There are some commands missing. The solution should starts with
+mkdir secretDir
+touch .secret
+rm -r maliciousFiles/
+...
+...
+...
