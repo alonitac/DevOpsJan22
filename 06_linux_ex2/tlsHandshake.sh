9e3f94a832a9bf6a0a63ac9f57f0f51d4c92210d
alexeymihaylovDev <alexeymihaylovDev@gmail.com>
2022-03-23 19:49:12 +0200

    exam

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,16 +1,36 @@
-curl -i -X POST -H "Content-Type: application/json" -d '{"clientVersion":"3.2","message":"Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello
-curl  -X POST -H "Content-Type: application/json" -d '{"clientVersion":"3.2","message":"Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello | jq -r '.serverCert' > cert.pem
+#!/bin/bash
 
+curl  -X POST -H "Content-Type: application/json" -d '{"clientVersion":"3.2","message":"Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello | jq -r '.serverCert' > cert.pem
+curl  -X POST -H "Content-Type: application/json" -d '{"clientVersion":"3.2","message":"Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello | jq -r '.sessionID' > sessionID.txt
+
+#dowload Amazon Web Services cer
+wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+
+#verify the certificate
 VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
   exit 1
 fi
-openssl rand -base64 32 >masterKey.txt
 
-SESSION_ID=$(cat sessionID)
-MASTER_KEY=$(cat masterKey.txt)
-curl -i -X POST -H "Content-Type: application/json" -d '{"sessionID":"'${SESSION_ID}'","masterKey":"'${MASTER_KEY}'","sampleMessage":"Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange
+#generate a 32 random bytes string
+openssl rand -base64 32 > masterKey.txt
 
+#request to the server endpoint /keyexchange
+SESSION_ID=$(cat sessionID.txt)
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc  -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+curl -X POST -H "Content-Type: application/json" -d '{"sessionID":"'${SESSION_ID}'","masterKey":"'${MASTER_KEY}'","sampleMessage":"Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange | jq -r '.encryptedSampleMessage' > encryptedSampleMessage.txt
 
+#ncode it to binary
+cat encryptedSampleMessage.txt | base64 -d  > encSampleMsgReady.txt
+#decript massage
+openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out dencryptedSampleMessage.txt
+
+DECRYPTED_SAMPLE_MESSAGE=$(cat dencryptedSampleMessage.txt)
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+  echo "Server symmetric encryption using the exchanged master-key has failed."
+  exit 1
+else
+  echo "Client-Server TLS handshake has been completed successfully"
+fi
 
