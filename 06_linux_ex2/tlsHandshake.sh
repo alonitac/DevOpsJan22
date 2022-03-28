65b28d26bc134defcb9251bb4456b74a2f049922
ami-porat <poratnick@gmail.com>
2022-03-28 22:07:05 +0300

    WIP

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,9 +1,5 @@
-#TLS communication
-#Step 1 - Client Hello (Client -> Server).
 
 curl -X POST -H 'Content-Type: application/json' -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello
-
-#Step 2 - Server Hello (Server -> Client)
 
 curl -X POST -H 'Content-Type: application/json' -d '{"clientVersion": "3.2", "message": "Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello | jq -r '.serverCert' > cert.pem
 
@@ -13,17 +9,12 @@ wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 
 openssl verify -CAfile cert-ca-aws.pem cert.pem
 
-#cert.pem: OKVERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
+VERIFICATION_RESULT=$(openssl verify -CAfile cert-ca-aws.pem cert.pem)
 
-if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]
-then
-
+if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
   exit 1
-
 fi
-
-# create mastekey.txt 32 bytes file
 
 dd if=/dev/urandom of=masterKey.txt bs=32 count=1
 
@@ -37,12 +28,9 @@ cat encryptedSampleMessage.txt | base64 -d > encryptedSampleMsgReady.txt
 
 openssl enc -d -aes-256-cbc -pbkdf2 -kfile masterKey.txt -in encSampleMsgReady.txt -out dencryptedSampleMassage.txt
 
-if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ];
-then
-
+DECRYPTED_SAMPLE_MESSAGE=$(cat dencryptedSampleMassage.txt)
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
   echo "Server symmetric encryption using the exchanged master-key has failed."
-exit
-
+  exit 1
   else echo "Client-Server TLS handshake has been completed successfully"
-
 fi
