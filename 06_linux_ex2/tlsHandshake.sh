bb1c5f80f48eef27c5abe6ac5db09bbeefbfa9d6
kostalubarsky <lubarsky1977@gmail.com>
2022-03-28 09:14:41 +0300

    Final Solution

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -15,7 +15,7 @@ MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER 
 curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange  | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
 openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out decrypted_secret.txt
-DECRYPTED_SAMPLE_MESSAGE=$(<decrypted_secret.txt)
+DECRYPTED_SAMPLE_MESSAGE=$(decrypted_secret.txt)
 if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
   echo "Server symmetric encryption using the exchanged master-key has failed."
   exit 1
