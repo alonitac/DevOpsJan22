684262abc776e60d3a6e48c66a509f7feb22d6f2
alonit <alon.itach@mail.huji.ac.il>
2022-03-29 16:08:00 +0300

    python katas

---
+++ python_katas/kata_1/questions.py
@@ -0,0 +1,191 @@
+def sum_of_element(elements):
+    """
+    1 Kata
+
+    :param elements: list of integers
+    :return: Return the resulting string.
+    """
+    s = 0
+    for num in elements:
+        s = s + num
+
+    return s
+
+
+def verbing(word):
+    """
+    1 Kata
+
+    Given a string 'word', if its length is at least 3, add 'ing' to its end.
+    Unless it already ends in 'ing', in which case add 'ly' instead.
+    If the string length is less than 3, leave it unchanged.
+
+    :param word: str
+    :return: Return the resulting string.
+    """
+    return None
+
+
+def words_concatenation(words):
+    """
+    1 Kata
+
+    Given a list of words, write a program that concatenates the words.
+
+    For example:
+    >> words_concatenation(['take', 'me', 'home'])
+       'take me home'
+
+    :param words: list of str
+    :return: Return the resulting string.
+    """
+    return None
+
+
+def reverse_words_concatenation(words):
+    """
+    1 Kata
+
+    Given a list of words, write a program that concatenates the words in a reverse way (both words and each word itself)
+
+    For example:
+    >> reverse_words_concatenation(['take', 'me', 'home'])
+       'home me take'
+
+    :param words: list of str
+    :return: Return the resulting string.
+    """
+    return None
+
+
+def is_unique_string(some_str):
+    """
+    1 Kata
+
+    :param some_str:
+    :return:
+    """
+    return None
+
+
+def list_diff(elements):
+    """
+    1 Kata
+
+    :param elements:
+    :return:
+    """
+    return None
+
+
+def prime_number(num):
+    """
+    1 Kata
+
+    hint: use the built-in function "range"
+    :param num:
+    :return:
+    """
+    return None
+
+
+def palindrome_num(num):
+    """
+    1 Kata
+
+    :param num:
+    :return:
+    """
+    return None
+
+
+def pair_match(men, women):
+    """
+    1 Kata
+
+    :param men:
+    :param women:
+    :return:
+    """
+    return None
+
+
+def bad_average():
+    """
+    1 Kata
+    :return:
+    """
+
+
+def best_student():
+    """
+    1 Kata
+
+
+    :return:
+    """
+    return None
+
+
+def print_dict_as_table(some_dict):
+    """
+    1 Kata
+
+
+    :return:
+    """
+    return None
+
+
+def merge_dicts(some_dict):
+    """
+    1 Kata
+
+
+    :return:
+    """
+    return None
+
+
+def seven_boom():
+    """
+    1 Kata
+
+    :return:
+    """
+    return None
+
+
+def ceaser_cipher():
+    """
+    1 Kata
+
+    :return:
+    """
+    return None
+
+
+def sum_of_digits():
+    """
+    1 Kata
+
+    :return:
+    """
+    return None
+
+
+if __name__ == '__main__':
+
+    print('\nSum of elements exercise results:\n')
+    print(sum_of_element([1, 2, 3, 4, 5, 6]))
+
+    print('\nVerbing exercise results:\n')
+    print(verbing('walk'))
+    print(verbing('swimming'))
+    print(verbing('do'))
+
+
+
+
+
+
---
+++ python_katas/kata_1/test.py
@@ -0,0 +1,25 @@
+import unittest
+from python_katas.kata_1 import questions
+
+
+class TestSumOfElements(unittest.TestCase):
+
+    def test_empty_list(self):
+        lst = []
+        self.assertEqual(questions.sum_of_element(lst), 0)
+
+    def test_integers_list(self):
+        lst = [1, 2, 3, 4, 5]
+        self.assertEqual(questions.sum_of_element(lst), 15)
+
+    def test_negative_numbers(self):
+        lst = [1, -6, 7, 0, 99]
+        self.assertEqual(questions.sum_of_element(lst), 101)
+
+    def test_all_zeros(self):
+        lst = [0] * 50000
+        self.assertEqual(questions.sum_of_element(lst), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
