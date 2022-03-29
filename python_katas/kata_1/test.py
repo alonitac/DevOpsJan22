1103136c173fc29c635ff819855029cd7ed88a99
alonit <alon.itach@mail.huji.ac.il>
2022-03-29 17:46:40 +0300

    python katas

--- python_katas/kata_1/test.py
+++ python_katas/kata_1/test.py
@@ -21,5 +21,24 @@ class TestSumOfElements(unittest.TestCase):
         self.assertEqual(questions.sum_of_element(lst), 0)
 
 
+class TestSumOfElements2(unittest.TestCase):
+
+    def test_empty_list(self):
+        lst = []
+        self.assertEqual(questions.sum_of_element(lst), 0)
+
+    def test_integers_list(self):
+        lst = [1, 2, 3, 4, 5]
+        self.assertEqual(questions.sum_of_element(lst), 15)
+
+    def test_negative_numbers(self):
+        lst = [1, -6, 7, 0, 99]
+        self.assertEqual(questions.sum_of_element(lst), 100)
+
+    def test_all_zeros(self):
+        lst = [0] * 50000
+        self.assertEqual(questions.sum_of_element(lst), 0)
+
+
 if __name__ == '__main__':
     unittest.main()
