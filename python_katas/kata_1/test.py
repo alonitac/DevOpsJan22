f03de0d709a995076e6371d1e221e078c4a18e10
alonit <alon.itach@mail.huji.ac.il>
2022-04-01 13:46:38 +0300

    python katas

--- python_katas/kata_1/test.py
+++ python_katas/kata_1/test.py
@@ -3,6 +3,15 @@ from python_katas.kata_1 import questions
 from python_katas.utils import unittest_runner
 
 
+testers = ['dorondollev',
+           'danielbar0101',
+           'DustyMadDude',
+           'Amitpoz',
+           'netanalm',
+           'AlexeyMihaylovDev',
+           'xXxARLxXx']
+
+
 class TestSumOfElements(unittest.TestCase):
     """
     1 Katas
@@ -24,26 +33,154 @@ class TestSumOfElements(unittest.TestCase):
         self.assertEqual(questions.sum_of_element(lst), 0)
 
 
-class TestSumOfElements2(unittest.TestCase):
+class TestVerbing(unittest.TestCase):
     """
     1 Katas
     """
 
-    def test_empty_list(self):
-        lst = []
-        self.assertEqual(questions.sum_of_element(lst), 0)
+    def test_sample(self):
+        # your code here
+        pass
 
-    def test_integers_list(self):
-        lst = [1, 2, 3, 4, 5]
-        self.assertEqual(questions.sum_of_element(lst), 15)
 
-    def test_negative_numbers(self):
-        lst = [1, -6, 7, 0, 99]
-        self.assertEqual(questions.sum_of_element(lst), 101)
+class TestWordsConcatenation(unittest.TestCase):
+    """
+    1 Katas
+    """
 
-    def test_all_zeros(self):
-        lst = [0] * 50000
-        self.assertEqual(questions.sum_of_element(lst), 0)
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestReverseWordsConcatenation(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestIsUniqueString(unittest.TestCase):
+    """
+    2 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestListDiff(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPrimeNumber(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPalindromeNum(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPairMatch(unittest.TestCase):
+    """
+    3 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestBadAverage(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestBestStudent(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestPrintDictAsTable(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestMergeDicts(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestSevenBoom(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestCaesarCipher(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
+
+
+class TestSumOfDigits(unittest.TestCase):
+    """
+    1 Katas
+    """
+
+    def test_sample(self):
+        # your code here
+        pass
 
 
 if __name__ == '__main__':
