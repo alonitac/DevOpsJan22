a138fc986f11fb952fc8d5630a992d4f7ca25425
Daniel shainfeld <shainfeldd@gmail.com>
2022-04-02 23:10:54 +0300

    daniel's solution

---
+++ 06_linux_ex2/tlsHandshake.sh
@@ -0,0 +1,13 @@
+sudo apt install jq
+curl -X POST -H 'Content-Type: application/json' -d '{"clientVersion": "3.2","message": "Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello -o fromserver.json
+SESSION_ID=$(jq -r '.sessionID' fromserver.json)
+jq -r '.serverCert' fromserver.json > cert.pem
+wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem cert.pem
+openssl rand -hex 16 > masterKey
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange  | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
+openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out decrypted_secret.txt
+cat decrypted_secret.txt
+
