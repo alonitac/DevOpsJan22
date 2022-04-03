5628d0eeeb43e5dcb2031bf5f13c378d74b610d1
kostalubarsky <lubarsky1977@gmail.com>
2022-04-03 17:10:52 +0300

    kata number 3

--- python_katas/kata_1/MY_Answers.py
+++ python_katas/kata_1/MY_Answers.py
@@ -11,14 +11,34 @@ sum_of_element(elements_for_function[1,2,3,4,5])
 print(s)
 '''
 
-any_word = 'wording'
-count_char = len(any_word)
-print('Length of your word is ',len(any_word))
-if any_word[-3:]=='ing':
-    print('Three last characters is ing ')
-    print(any_word + 'ly')
-elif count_char >= 3:
-    any_word = any_word + ('ing')
-    print(any_word)
-elif count_char < 3:
-    print(any_word)
+'''
+def verbing(word):
+
+    count_char = len(word)
+    print('Length of your word is',len(word), 'characters')
+    if word[-3:]=='ing':
+            print('Three last characters is ing ')
+            word = word + 'ly'
+    elif count_char >= 3:
+            word = word + ('ing')
+    elif count_char < 3:
+            return word
+    return word
+verb ='fud'
+print(verbing(verb))
+'''
+
+
+'''
+def words_concatenation(words):
+    concat_string = ' '.join(words)
+    print(concat_string)
+words_concatenation(['take','me','home','now'])
+'''
+
+
+
+
+
+
+
