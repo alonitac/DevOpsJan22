41883965da734e5eaf81d3cf931bcbc02c0a4630
kostalubarsky <lubarsky1977@gmail.com>
2022-04-03 14:38:18 +0300

    kata number 2

--- python_katas/kata_1/MY_Answers.py
+++ python_katas/kata_1/MY_Answers.py
@@ -1,25 +1,24 @@
 '''
-elements_for_function = [-1,2,3,4,5,6]
+#elements_for_function = [-1,2,3,4,5,6]
 def sum_of_element(elements):
     s = 0
 
     for num in elements:
         s = s + num
 
-    print(s)
-sum_of_element(elements_for_function)
+    return s
+sum_of_element(elements_for_function[1,2,3,4,5])
+print(s)
 '''
 
-any_word = 'doing'
+any_word = 'wording'
 count_char = len(any_word)
 print('Length of your word is ',len(any_word))
-if  count_char >= 3:
-    any_word = any_word + ('ing')
-    print(any_word)
-#elif count_char < 3:
-#    print(any_word)
-elif any_word[-3:] == 'ing':
+if any_word[-3:]=='ing':
     print('Three last characters is ing ')
     print(any_word + 'ly')
+elif count_char >= 3:
+    any_word = any_word + ('ing')
+    print(any_word)
 elif count_char < 3:
     print(any_word)
