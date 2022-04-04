e9265eb1afcf283f961a96ff027f8b670dbc0a76
kostalubarsky <lubarsky1977@gmail.com>
2022-04-04 16:52:28 +0300

    including prime_number kata

--- python_katas/kata_1/MY_Answers.py
+++ python_katas/kata_1/MY_Answers.py
@@ -31,11 +31,73 @@ print(verbing(verb))
 
 '''
 def words_concatenation(words):
-    concat_string = ' '.join(words)
-    print(concat_string)
-words_concatenation(['take','me','home','now'])
+    if words==[]:
+        print('Your list is empty,BYE')
+        return None
+    else:
+        concat_string = ' '.join(words)
+        print(concat_string)
+words_concatenation(['take','me'])
 '''
 
+'''
+def reverse_words_concatenation(words):
+    if words==[]:
+        print('Your list is empty,BYE')
+        return None
+    else:
+        concat_string = ' '.join(words)
+        words = concat_string.reverse()
+        print(words)
+    return words
+reverse_words_concatenation(['take','me'])
+'''
+
+'''
+def is_unique_string(some_str):
+  for a in range(len(some_str)):
+    for b in range(a + 1,len(some_str)):
+      if(some_str[a] == some_str[b]):
+        print('False')
+        return False
+  print('True')
+  return True
+is_unique_string('12341')
+'''
+
+'''
+def list_diff(elements): !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    i = elements[0]-elements[0]
+    j = elements
+    print(i)
+    #for i in elements:
+     #   print(j-i)
+
+list_diff([1,15,20,30])
+'''
+
+'''
+def prime_number(num):
+
+    boolean = False
+
+# 0 to 3 is prime numbers
+    if num > 3:
+        # check if num is exactly divisible by any number from 2 to num - 1
+        for i in range(2, num-1):
+            if (num % i) == 0:
+              boolean = True
+              break
+
+    if boolean==True:
+        print(num, "is not a prime number")
+        return False
+    else:
+      print(num, "is a prime number")
+      return True
+
+prime_number(15)
+'''
 
 
 
