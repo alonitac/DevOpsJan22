cb85a33c70280d8ded98883c941f11bc400da298
alonit <alon.itach@mail.huji.ac.il>
2022-04-06 14:31:27 +0300

    katas wip and fixes

--- python_katas/kata_1/questions.py
+++ python_katas/kata_1/questions.py
@@ -65,7 +65,7 @@ def is_unique_string(some_str):
     """
     2 Kata
 
-    Given a string, the function returns True is all characters in the string are unique, False otherwise
+    Given a string, the function returns True if all characters in the string are unique, False otherwise
 
     e.g
     'abcd' -> True
@@ -86,7 +86,7 @@ def list_diff(elements):
     reduces by its previous one. The first element should be None
 
     e.g.
-    [1, 2, 3, 4, 7, 11] -> [None, 1, 1, 3, 4]
+    [1, 2, 3, 4, 7, 11] -> [None, 1, 1, 1, 3, 4]
     [] -> []
     [1, 5, 0, 4, 1, 1, 1] -> [None, 4, -5, 4, -3, 0, 0]
 
---
+++ python_katas/kata_3/questions.py
@@ -0,0 +1,86 @@
+def knapsack():
+    pass
+
+
+def time_me():
+    pass
+
+
+def youtube_download():
+    pass
+
+
+def visualizer():
+    pass
+
+
+def job_scheduling():
+    pass
+
+
+def valid_git_graph():
+    pass
+
+
+def rotate_img():
+    pass  # use rotate matrix
+
+
+def img_blur():
+    pass  # matrix_avg
+
+
+def traceroute_parser():
+    pass  # regexp
+
+
+def apache_logs_parser():
+    return 1, 2, 3, 4, 5, 6  # return all values
+
+
+def simple_http_request():
+    pass
+
+
+def pyjwt_demo():
+    pass
+
+
+def pyaudio():
+    pass
+
+
+class SortedDict:
+    pass
+
+
+class CacheDict:
+    pass
+
+# pylint
+
+
+class SomeDir:
+
+    def __init__(self):
+        pass
+
+    def get_parent(self):
+        pass
+
+    def move_to_parent(self):
+        pass
+
+    def del_book(self):
+        pass
+
+    def create_book(self):
+        pass
+
+
+class IpScanner:
+    pass
+
+
+
+
