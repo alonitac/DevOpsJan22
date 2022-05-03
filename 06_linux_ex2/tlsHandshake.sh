284c2fb99c20ce7c6d509c73d478f5b68e78ae64
alonit <alon.itach@mail.huji.ac.il>
2022-05-03 14:50:03 +0300

    Linux ex2 feedback

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -4,10 +4,16 @@ SESSION_ID=$(jq -r '.sessionID' fromserver.json)
 jq -r '.serverCert' fromserver.json > cert.pem
 wget https://devops-jan22.s3.eu-north-1.amazonaws.com/cert-ca-aws.pem
 openssl verify -CAfile cert-ca-aws.pem cert.pem
+
+# FIXME missing checking that the certificate is verified successfully -5
+
 openssl rand -hex 16 > masterKey
 MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
 curl -X POST -H 'Content-Type: application/json' -d '{"sessionID": "'$SESSION_ID'","masterKey": "'$MASTER_KEY'","sampleMessage": "Hi server, please encrypt me and send to client!"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/keyexchange  | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
 openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out decrypted_secret.txt
 cat decrypted_secret.txt
+
+# FIXME missing checking that the message was encrypted successfully -5
+
 
