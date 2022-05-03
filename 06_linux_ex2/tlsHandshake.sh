5e9331b8e73a1eb65e8b9561bc7f78a32e43562b
alonit <alon.itach@mail.huji.ac.il>
2022-05-03 14:58:10 +0300

    Linux ex2 feedback

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,3 +1,5 @@
+# FIXME Great!
+
 curl -X POST -H 'Content-Type: application/json' -d '{"clientVersion": "3.2","message": "Client Hello"}' http://devops-jan22-1273001359.eu-north-1.elb.amazonaws.com:8080/clienthello -o fromserver.json
 sudo apt install jq
 SESSION_ID=$(jq -r '.sessionID' fromserver.json)
