5b7f317852496e989a7e95b643ebe21ed2b717ae
ami-porat <poratnick@gmail.com>
2022-05-10 21:23:38 +0300

    end_git_exercises

--- python_katas/kata_1/questions.py
+++ python_katas/kata_1/questions.py
@@ -193,7 +193,22 @@ def pair_match(men, women):
     :return: tuple (men_name, women_name) such their age absolute difference is the minimal
     """
 
-    return
+    def pair_match(men, women):
+
+        age_min = 99999
+        names_min = ("a", "b")
+
+        for key, value in men.items():
+            for w_key, w_value in women.items():
+
+                d_keys = (key, w_key)
+                age_deff = abs(value - w_value)
+                if age_min > age_deff:
+                    names_min = d_keys
+                    age_min = age_deff
+
+        return names_min
+
 
 def bad_average(a, b, c):
     """
