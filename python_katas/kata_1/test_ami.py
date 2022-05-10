da08f542e51ad5ccf8cd37a981b507f5359774d7
ami-porat <poratnick@gmail.com>
2022-05-10 21:23:52 +0300

    end_git_exercises

--- python_katas/kata_1/test_ami.py
+++ python_katas/kata_1/test_ami.py
@@ -1,16 +1,39 @@
-d = {"":""}
-man_list = list(d.values())
-women_list = list(d.values())
-man_l_k = list(d.keys())
-women_l_k = list(d.keys())
+def pair_match(men, women):
+
+    age_min = 99999
+    names_min = ("a", "b")
+
+    for key, value in men.items():
+        for w_key, w_value in women.items():
+
+            d_keys = (key, w_key)
+            age_deff = abs(value - w_value)
+            if age_min > age_deff:
+
+                names_min = d_keys
+                age_min = age_deff
+
+    return names_min
+
 
 men = {"John": 20, "Abraham": 45}
 women = {"July": 18, "Kim": 28}
 
-for man_list in d:
+print(pair_match(men, women))
 
-abs(John - Kim) = abs(20 - 26) = abs(-6) = 6
-abs(John - July) = abs(20 - 18) = abs(2) = 2
-abs(Abraham - Kim) = abs(45 - 26) = abs(19) = 19
-abs(Abraham - July) = abs(45 - 18) = abs(27) = 27
-
+    #
+# d = {"":""}
+# man_list = list(d.values())
+# women_list = list(d.values())
+# man_l_k = list(d.keys())
+# women_l_k = list(d.keys())
+#
+#
+#
+# for man_list in d:
+#
+# abs(John - Kim) = abs(20 - 26) = abs(-6) = 6
+# abs(John - July) = abs(20 - 18) = abs(2) = 2
+# abs(Abraham - Kim) = abs(45 - 26) = abs(19) = 19
+# abs(Abraham - July) = abs(45 - 18) = abs(27) = 27
+#
