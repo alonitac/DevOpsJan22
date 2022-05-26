ff6b3dbc27688eda7b2dedece5529949eaa776c5
kostalubarsky <lubarsky1977@gmail.com>
2022-05-26 15:12:59 +0300

    Test kata 2 for "string compression" commit

--- python_katas/kata_1/MY_Answers.py
+++ python_katas/kata_1/MY_Answers.py
@@ -365,3 +365,11 @@ while True:
 if flag == -1:
     print("Not a Valid Password")
 '''
+
+string = 'Python for begginners'
+print(string.upper())
+print(string.lower())
+print(string.find('n'))
+print(string.count('n'))
+print(string.replace('n','m'))
+print(string.title())
