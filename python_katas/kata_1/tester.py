fa3a629314ca8f27c1e810613330f0e7940c9037
ami-porat <poratnick@gmail.com>
2022-05-31 20:55:26 +0300

    wif

---
+++ python_katas/kata_1/tester.py
@@ -0,0 +1,18 @@
+def pair_match(men, women):
+    def pair_match(men, women):
+
+        age_min = 99999
+        names_min = ("a", "b")
+
+        for key, value in men.items():
+            for w_key, w_value in women.items():
+
+                d_keys = (key, w_key)
+                age_deff = abs(value - w_value)
+                if age_min > age_deff:
+                    names_min = d_keys
+                    age_min = age_deff
+
+        return names_min
+
+print(pair_match(''))
