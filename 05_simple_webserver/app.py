fa0f0875cf0c12b1fc146e078751947d6a1eec9d
alonit <alon.itach@mail.huji.ac.il>
2022-07-10 15:54:08 +0300

    aws application load balancer

--- 05_simple_webserver/app.py
+++ 05_simple_webserver/app.py
@@ -1,6 +1,9 @@
+import os
 from flask import Flask, send_file, request
 
 app = Flask(__name__)
+
+hostname = os.environ.get('HOSTNAME', None)
 
 
 # Try by:  curl localhost:8080
@@ -21,6 +24,20 @@ def update_profile():
 @app.route('/get-profile-picture')
 def profile_picture():
     return send_file('images/profile-1.jpg', mimetype='image/gif')
+
+
+@app.route('/status')
+def status():
+    return 'OK'
+
+
+@app.route('/load-test')
+def load_test_endpoint():
+    x = 6
+    for i in range(100000):
+        x = x ** i
+
+    return f'Done {hostname}'
 
 
 if __name__ == '__main__':
