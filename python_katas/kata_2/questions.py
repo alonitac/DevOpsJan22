e12764ee98cdc9d7d6a333a75efca894a002ac12
DanielAdmin <prodevildaniel@gmail.com>
2022-08-24 20:23:07 +0300

    working on kata 2

--- python_katas/kata_2/questions.py
+++ python_katas/kata_2/questions.py
@@ -13,7 +13,23 @@ def valid_parentheses(s):
     s = '[[{()}](){}]'  -> True
     s = '[{]}'          -> False
     """
-    return None
+    open_list = ["[", "{", "("]
+    close_list = ["]", "}", ")"]
+    stack = []
+    for i in s:
+        if i in open_list:
+            stack.append(i)
+        elif i in close_list:
+            pos = close_list.index(i)
+            if ((len(stack) > 0) and
+                    (open_list[pos] == stack[len(stack) - 1])):
+                stack.pop()
+            else:
+                return "Invalid"
+    if len(stack) == 0:
+        return "Valid"
+    else:
+        return "Invalid"
 
 
 def fibonacci_fixme(n):
