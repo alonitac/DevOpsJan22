6def458107520242f5ed1b4d2b421ac6b4b89a04
ami-porat <poratnick@gmail.com>
2022-09-03 22:22:17 +0300

    kata3 end of life

--- python_katas/kata_3/questions.py
+++ python_katas/kata_3/questions.py
@@ -121,30 +121,7 @@ class SortedDict(dict):
 
 
 class CacheList(list):
-    """
-    8 Kata
 
-    Implement CacheList class which is a regular Python list,
-    but it holds the last n elements only (old elements will be deleted)
-
-    Usage example:
-    x = CacheList(3)
-
-    x.append(1)
-    x.append(2)
-    x.append(3)
-
-    print(x)
-    >> [1, 2, 3]
-
-    x.append(1)
-    print(x)
-    >> [2, 3, 1]
-
-    x.append(1)
-    print(x)
-    >> [3, 1, 1]
-    """
     def __init__(self, cache_size=5):
         super().__init__()
         pass
