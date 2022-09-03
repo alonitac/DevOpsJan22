c6876ca423455dce4166892ef6668cb62c45f949
ami-porat <poratnick@gmail.com>
2022-09-03 21:19:53 +0300

    kata3 end of life

--- python_katas/kata_3/questions.py
+++ python_katas/kata_3/questions.py
@@ -5,22 +5,21 @@ ISO_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
 
 
 def knapsack(items, knapsack_limit=50):
-    """
-    5 Kata
+    l = {}
+    marklist = sorted(items.items(), key=lambda x:x[0] )
+    sortdict = dict(marklist)
+    count=0
+    sbs=int(knapsack_limit)
+    for key,value in sortdict,items():
+        count += value[0]
 
-    Consider a thief gets into a home to rob and he carries a knapsack.
-    There are fixed number of items in the home — each with its own weight and value —
-    Jewelry, with less weight and highest value vs tables, with less value but a lot heavy.
-    To add fuel to the fire, the thief has an old knapsack which has limited capacity.
-    Obviously, he can’t split the table into half or jewelry into 3/4ths. He either takes it or leaves it
+        if count <= sbs:
+            l.update({key:value})
+        if count > knapsack_limit:
+            break
+    print(l)
 
-    Given a set of items, dict of tuples representing the (weight, value), determine the items to include in a collection
-    so that the total weight is less than or equal to a given limit and the total value is as large as possible.
 
-    :param items: dict of tuples e.g. {"bed": (100, 15), "iphone13": (1, 1500)}
-    :param knapsack_limit:
-    :return: set of items
-    """
     return None
 
 
