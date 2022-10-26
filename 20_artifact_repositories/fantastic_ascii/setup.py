241281d788dc45ff99a0eff7bc21ce0372368698
alonit <alon.itach@mail.huji.ac.il>
2022-10-26 20:04:44 +0300

    fix version number

--- 20_artifact_repositories/fantastic_ascii/setup.py
+++ 20_artifact_repositories/fantastic_ascii/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup_args = dict(
     name='fantastic_ascii',
-    version='1.0.0',
+    version='1.1.0',
     description='Fantastic ASCII',
     license='MIT',
     install_requires=[
